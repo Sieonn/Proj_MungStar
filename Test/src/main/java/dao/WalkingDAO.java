537eeae989585dd8c0711ac2ec71d872f443dd07
yebrong <154613879+yebrong@users.noreply.github.com>
2024-04-09 18:22:31 +0900

    up

--- Test/src/main/java/dao/WalkingDAO.java
+++ Test/src/main/java/dao/WalkingDAO.java
@@ -8,13 +8,14 @@ import dto.Walking;
 public interface WalkingDAO {
 	void insertWalking(Walking walk) throws Exception;
 	List<Walking> selectWalkingList() throws Exception;
+	List<Walking> searchWalking(String walkAddress3, String searchText) throws Exception;
 	Walking selectWalking(Integer walkNum) throws Exception;
 	void updateWalking(Walking walk) throws Exception;
 
 	List<Comment> selectWalkComment(Integer walkNum) throws Exception;
 	
+	
 	void insertWalkComment(Comment comment) throws Exception;
 	
 	String selectWalkNick(String memId) throws Exception;
-	List<Walking> selectWalkListOnMain() throws Exception;
 }
--- Test/src/main/java/dao/WalkingDAOImpl.java
+++ Test/src/main/java/dao/WalkingDAOImpl.java
@@ -1,10 +1,13 @@
 package dao;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 
 import dto.Comment;
+import dto.Temp;
 import dto.Walking;
 import util.MybatisSqlSessionFactory;
 
@@ -33,7 +36,14 @@ public class WalkingDAOImpl implements WalkingDAO {
 	public Walking selectWalking(Integer walkNum) throws Exception{
 		return sqlSession.selectOne("mapper.walking.selectWalking",walkNum);
 	}
-	
+	@Override
+	public List<Walking> searchWalking(String walkAddress3, String searchText)
+			throws Exception {
+		Map<String,Object> param = new HashMap<>();
+		param.put("walkAddress3",walkAddress3);
+		param.put("searchText", searchText);
+		return sqlSession.selectList("mapper.walking.searchWalking",param);
+	}
 	@Override
 	public List<Comment> selectWalkComment(Integer walkNum) throws Exception {
 		return sqlSession.selectList("mapper.walking.selectWalkComment",walkNum);
@@ -46,10 +56,6 @@ public class WalkingDAOImpl implements WalkingDAO {
 	@Override
 	public String selectWalkNick(String memId) throws Exception {
 		return sqlSession.selectOne("mapper.walking.selectWalkNick",memId);
-	}
-	@Override
-	public List<Walking> selectWalkListOnMain() throws Exception {
-		return sqlSession.selectList("mapper.walking.selectWalkListOnMain");
 	}
 	
 }
