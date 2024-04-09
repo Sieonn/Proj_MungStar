54d381831be6b4e7da51fb1b3e8fbdb948788347
yebrong <154613879+yebrong@users.noreply.github.com>
2024-04-09 18:23:04 +0900

    up

---
+++ Test/src/main/java/controller/walkingSearch.java
@@ -0,0 +1,67 @@
+package controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.google.gson.Gson;
+
+import dto.Member;
+import dto.Walking;
+import service.WalkingService;
+import service.WalkingServiceImpl;
+
+/**
+ * Servlet implementation class walkingSearch
+ */
+@WebServlet("/walking/walkingSearch")
+public class walkingSearch extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public walkingSearch() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setCharacterEncoding("utf-8");
+		String searchText=request.getParameter("searchText");
+		System.out.println(searchText);
+		try {
+			WalkingService walkingService = new WalkingServiceImpl();
+			List<Walking> walkings = walkingService.searchWalking(request);
+//			Gson gson = new Gson();
+//			String walkings = gson.toJson(walking);
+//			response.getWriter().write(walkings);
+//			System.out.println(walkings);
+			request.setAttribute("searchText", searchText);
+			request.setAttribute("walkings",walkings);
+			request.getRequestDispatcher("walkingSearch.jsp").forward(request, response);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
